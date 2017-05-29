052ce950fe8ae4f11ea211520efb77d355668def
Chris <chris.huynh.c@gmail.com>
2017-05-28 22:04:40 -0500

    Added book search

--- scraper.js
+++ scraper.js
@@ -82,7 +82,45 @@ exports.redditScrape = function(url, cb){
 	});
 };
 
+function getBooks(searchText){
 
+    //var searchText = $('#input').val();
+
+    axios.get('http://api.tbp.io/search.json?q=' + searchText)
+        .then(function(response) {
+            console.log(response);
+            var books = response.data.books;
+            var output = '';
+
+            /*
+            var info = {
+                title: books[0].title,
+                isbn: books[0].isbn,
+                author: books[0].author
+
+            }
+            */
+            //console.log(info);
+            
+            $.each(books, function(index, book){
+                output += `
+                    <div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="${book.images.large}">
+                            <h5>${book.title}</h5>
+                            <a onclick="movieSelected('${book.isbn}')" class="btn btn-primary" href="#">Compare Prices</a>
+                        </div>
+                    </div>
+                `;
+            });
+            
+            $('#books').html(output);
+          
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+};
 
 
 
